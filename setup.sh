27457ea939f0eca5487324afe3fe6948dbe82c9c
fgken <tmp@tmp.com>
2014-12-17 16:16:35 +0900

    Setup NeoBundle.vim

--- setup.sh
+++ setup.sh
@@ -2,8 +2,16 @@
 cd `dirname $0`
 path=`pwd`
 
+# Install NeoBundle.vim
+if [ ! -e ~/.vim/bundle/neobundle.vim ]; then
+	echo "Install NeoBundle.vim"
+	curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+fi
+
+echo "Replace .vimrc"
 ln -sf $path/vimrc ~/.vimrc
 
+echo "Add my .bashrc"
 ln -sf $path/bashrc ~/.bashrc_fgken
 FGKENRC=". ~/.bashrc_fgken"
 if [ ! -e ~/.bashrc -o -z "`grep '# --- fgken bashrc ---' ~/.bashrc`" ]; then
